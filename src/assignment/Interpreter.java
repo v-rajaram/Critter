fcdee0e80b364ed7e342ebcf4373c9fdc2c190f9
Rohan Nair <mercury@wireless-10-147-91-85.public.utexas.edu>
2017-09-19 16:58:57 -0500

    Style changes

--- src/assignment/Interpreter.java
+++ src/assignment/Interpreter.java
@@ -1,63 +1,47 @@
 package assignment;
 
-
-
 import java.io.FileReader;
-
 import java.io.IOException;
-
 import java.util.Scanner;
 
-
-
 /**
-
  * Responsible for loading critter species from text files and interpreting the
-
  * simple Critter language.
-
  * 
-
  * For more information on the purpose of the below two methods, see the
-
  * included API/ folder and the project description.
-
  */
 
 public class Interpreter implements CritterInterpreter {
 
-
-
-public void executeCritter(Critter c) {
-
-// obviously, your code should do something
-
-return;
-
-}
-
-
-
-public CritterSpecies loadSpecies(String filename) throws IOException {
-// obviously, your code should do something
+	public void executeCritter(Critter c) {
 	
-	Scanner scan = new Scanner(new FileReader(filename));
-	String commandString = "";
-	String name = "";
+	// obviously, your code should do something
 	
-	if(scan.hasNextLine() && !(scan.nextLine().charAt(0) == '\n')) {
-		name = scan.nextLine();
+	return;
+	
 	}
-	
-	while(scan.hasNextLine()) {
-		commandString += scan.nextLine() + "%"; //Signifies Command endline
-	}
-	
-	String [] commands = commandString.split("%");
-	// Can you see this?
-	System.out.println(commands);
-	return null;
 
-}
+	public CritterSpecies loadSpecies(String filename) throws IOException {
+	// obviously, your code should do something
+		
+		Scanner scan = new Scanner(new FileReader(filename));
+		String commandString = "";
+		String name = "";
+		
+		if(scan.hasNextLine() && !(scan.nextLine().charAt(0) == '\n')) {
+			name = scan.nextLine();
+		}
+		
+		while(scan.hasNextLine()) {
+			commandString += scan.nextLine() + "%"; //Signifies Command endline
+		}
+		
+		String [] commands = commandString.split("%");
+		// Can you see this?
+		System.out.println(commands);
+		return null;
+	
+	}
 
 }
