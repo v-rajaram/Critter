d136a2f3142302d49aea8e7a041f7e4590a14217
Varun Rajaram <varun.rajaram@utexas.edu>
2017-09-19 15:36:49 -0500

    initial commit

---
+++ src/assignment/Interpreter.java
@@ -0,0 +1,23 @@
+package assignment;
+
+import java.io.IOException;
+
+/**
+ * Responsible for loading critter species from text files and interpreting the
+ * simple Critter language.
+ * 
+ * For more information on the purpose of the below two methods, see the
+ * included API/ folder and the project description.
+ */
+public class Interpreter implements CritterInterpreter {
+
+	public void executeCritter(Critter c) {
+		// obviously, your code should do something
+		return;
+	}
+
+	public CritterSpecies loadSpecies(String filename) throws IOException {
+		// obviously, your code should do something
+		return null;
+	}
+}
