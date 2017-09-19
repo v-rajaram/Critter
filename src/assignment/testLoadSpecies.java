0800eb6d375cf16c7de33787f879a25532018a5d
Rohan Nair <mercury@wireless-10-147-91-85.public.utexas.edu>
2017-09-19 17:28:32 -0500

    Fixed some paths for testing

--- src/assignment/testLoadSpecies.java
+++ src/assignment/testLoadSpecies.java
@@ -8,24 +8,39 @@ public class testLoadSpecies {
 	
 	
 	public static void main (String args[]) throws FileNotFoundException {
+		
+		
+		System.out.println("Enter in file pls");
 		Scanner user = new Scanner(System.in);
 		String fileName = user.nextLine();
-		Scanner scan = new Scanner(new FileReader(fileName));
+		
+		Scanner scan = new Scanner(new FileReader("./species/" + fileName));
 		String commandString = "";
 		String name = "";
 		
-		if(scan.hasNextLine() && !(scan.nextLine().charAt(0) == '\n')) {
-			name = scan.nextLine();
+		try {
+			
+			/**
+			if(scan.hasNextLine() && !(scan.nextLine().charAt(0) == '\n')) {
+				name = scan.nextLine();
+			}
+			
+			while(scan.hasNextLine()) {
+				commandString += scan.nextLine() + "%"; //Signifies Command endline
+			}
+			
+			String [] commands = commandString.split("%");
+			// Can you see this?
+			System.out.println(commands);
+			*/
+			while(scan.hasNextLine()) {
+				System.out.println(scan.nextLine());//Signifies Command endline
+			}
 		}
-		
-		while(scan.hasNextLine()) {
-			commandString += scan.nextLine() + "%"; //Signifies Command endline
+		finally {
+			user.close();
+			scan.close();
 		}
-		
-		String [] commands = commandString.split("%");
-		// Can you see this?
-		System.out.println(commands);
-		
 		
 	}
 	
